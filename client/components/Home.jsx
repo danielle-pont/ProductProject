aad38405053b510e1d7651e04a9ba39286e6a583
daneielle-pont <danielleshereepont@outlook.com>
2022-06-12 22:28:20 +1200

    fill out home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -8,6 +8,15 @@ function Product() {
       <header className="header">
         <h1>Product Project</h1>
       </header>
+      <header className="header">
+        <h1>Product Project</h1>
+      </header>
+      <header className="header">
+        <h1>Product Project</h1>
+      </header>
+      <header className="header">
+        <h1>Product Project</h1>
+      </header>
       <section className="main"></section>
       <footer className="footer"></footer>
     </>
